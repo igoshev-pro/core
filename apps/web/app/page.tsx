bff325c1b45f82de31e3abb4d4adfbb485689cea
Dimitri Igoshev <dimi.igoshev@gmail.com>
2025-12-18 01:26:57 +0300

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -11,11 +11,11 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
+        <span className={title()}>Делай&nbsp;</span>
+        <span className={title({ color: "violet" })}>красивые&nbsp;</span>
         <br />
         <span className={title()}>
-          websites regardless of your design experience.
+          вебсайты.
         </span>
         <div className={subtitle({ class: "mt-4" })}>
           Beautiful, fast and modern React UI library.
