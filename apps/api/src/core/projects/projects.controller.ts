f7bd4076750bbd6da9a540e221b4ca9a94ef3a69
Dimitri Igoshev <61008146+Dimitri-Igoshev@users.noreply.github.com>
2025-12-22 11:17:01 +0300

    Update projects.controller.ts

--- apps/api/src/core/projects/projects.controller.ts
+++ apps/api/src/core/projects/projects.controller.ts
@@ -17,18 +17,18 @@ import { Me } from '../auth/me.decorator'
 import { UserRole } from 'src/common/enums/user.unum'
 import { ClientGuard } from '../clients/client.guard';
 
-// @UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard)
 @Controller('core/projects')
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
 
-  // @UseGuards(ClientGuard)
+  @UseGuards(ClientGuard)
   @Post()
   create(@Body() createProjectDto: CreateProjectDto) {
     return this.projectsService.create(createProjectDto);
   }
 
-  // @UseGuards(ClientGuard)
+  @UseGuards(ClientGuard)
   @Get()
   findAll(@Query() query: Record<string, string>, @Me() user: any) {
     if (user.role !== UserRole.SuperAdmin) {
@@ -37,13 +37,13 @@ export class ProjectsController {
     return this.projectsService.findAll(query);
   }
 
-  // @UseGuards(ClientGuard)
+  @UseGuards(ClientGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.projectsService.findOne(id);
   }
 
-  // @UseGuards(ClientGuard)
+  @UseGuards(ClientGuard)
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -52,7 +52,7 @@ export class ProjectsController {
     return this.projectsService.update(id, updateProjectDto);
   }
 
-  // @UseGuards(ClientGuard)
+  @UseGuards(ClientGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.projectsService.remove(id);
