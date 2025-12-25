2757be4817251238672499ff8afdf4b8f633e91b
Dimitri Igoshev <dimi.igoshev@gmail.com>
2025-12-25 05:16:14 +0300

    Revert "Update main.ts"
    This reverts commit ef912055545887bd35118a1149060cb462b2944f.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,7 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      "https://igoshev.pro",
+      "https://www.igoshev.pro",
+      "http://localhost:3000",
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "x-project-id",
+    ],
+    exposedHeaders: ["ETag"],
+    credentials: false,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
