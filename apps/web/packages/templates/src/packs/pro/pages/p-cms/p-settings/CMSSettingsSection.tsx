cd90e67bdcbb16b92ec2bdf8ff15d3c9fb3d8a45
Dimitri Igoshev <dimi.igoshev@gmail.com>
2026-01-07 13:42:51 +0300

    Update CMSSettingsSection.tsx

--- apps/web/packages/templates/src/packs/pro/pages/p-cms/p-settings/CMSSettingsSection.tsx
+++ apps/web/packages/templates/src/packs/pro/pages/p-cms/p-settings/CMSSettingsSection.tsx
@@ -3,7 +3,7 @@ import CMSettingsSectionC from './CMSSettingsSectionC'
 
 const CMSSettingsSection = ({ node, ctx }: any) => {
   return (
-    <CMSettingsSectionC type={node.props.type} projectId={ctx.projectId} />
+    <CMSettingsSectionC projectId={ctx.projectId} />
   )
 }
 
